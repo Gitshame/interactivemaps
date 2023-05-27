6069cc491c73fa19611fd46b087a27ecb9a37025
Chris Hultin <chrishultin@google.com>
2023-05-27 20:38:55 +0000

    Adding image overlay

--- frontend/src/components/MapComponent.vue
+++ frontend/src/components/MapComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-
+    <l-map ref="map" :v-model:zoom="zoom" :center="[0,0]" crs=L.CRS.Simple>
+      <l-image-overlay url=mapsStore.getMap(props.map_id).image bounds=mapsStore.getMapBounds(props.map_id)></l-image-overlay>
+    </l-map>
   </div>
 </template>
 
@@ -13,6 +15,10 @@ import {
   toRef,
   Ref,
 } from 'vue';
+
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
 import { Todo, Meta } from './models';
 import { api } from 'boot/axios';
 import { useInteractiveMapStore } from 'stores/map-store'
@@ -28,7 +34,7 @@ export default defineComponent({
       .then((response) => {
         mapsStore.loadMaps(response.data)
       })
-    return { mapsStore };
+    return { mapsStore, props };
   },
 });
 </script>
--- frontend/src/stores/map-store.ts
+++ frontend/src/stores/map-store.ts
@@ -1,4 +1,6 @@
 import { defineStore } from 'pinia';
+import type L from "leaflet";
+import { computed, ref } from "vue";
 
 export const useInteractiveMapStore = defineStore('interactive-maps', {
   state: () => ({
@@ -11,5 +13,20 @@ export const useInteractiveMapStore = defineStore('interactive-maps', {
     loadMaps(mapsList: []) {
       this.maps = mapsList;
     },
+    getMap(mapId: number) {
+      return this.maps.find((map) => map.id === mapId);
+    },
+    getMapBounds(mapId: number) {
+      const map = this.getMap(mapId);
+      const bounds = computed(
+        () =>
+          [
+            [0, 0],
+            [map.x_dimension, map.y_dimension],
+          ] as L.LatLngBoundsLiteral
+      );
+
+      return bounds;
+    }
   },
 });
